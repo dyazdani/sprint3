b0f9ec21bfa2ee9f42c8b2d153cd8bce48703d02
Dara Yazdani <darayazdani@Daras-MacBook-Air.local>
2023-01-19 10:35:35 -0800

    fixing keydown event to be in EvilCircle constructor

--- bouncing-balls/main.js
+++ bouncing-balls/main.js
@@ -81,23 +81,24 @@ class EvilCircle extends Shape {
     super(x, y, 20, 20);
     this.color = white;
     this.size = 10;
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
   }
-  window.addEventListener("keydown", (e) => {
-    switch (e.key) {
-      case "a":
-        this.x -= this.velX;
-        break;
-      case "d":
-        this.x += this.velX;
-        break;
-      case "w":
-        this.y -= this.velY;
-        break;
-      case "s":
-        this.y += this.velY;
-        break;
-    }
-  });
+  
   draw() {
     ctx.beginPath();
     ctx.strokeStyle = this.color;
